e259f8b2e07f805e6a75aeaa34fa45700f51f826
Irvin Funes <irvinfunes2002@gmail.com>
2024-09-11 19:30:03 -0600

    config v3

--- src/main/java/com/irvin/funes/rrhh/RrhhApplication.java
+++ src/main/java/com/irvin/funes/rrhh/RrhhApplication.java
@@ -14,21 +14,5 @@ public class RrhhApplication {
 		SpringApplication.run(RrhhApplication.class, args);
 	}
 
-	@Configuration
-	public static class Myconfiguration{
-		@Bean
-		public WebMvcConfigurer corsConfigurer(){
-			return new WebMvcConfigurer() {
-				@Override
-				public void addCorsMappings(CorsRegistry registry) {
-					registry.addMapping("/**")
-							.allowedOrigins("*")
-							.allowedHeaders("*")
-							.allowedMethods("*");
-
-				}
-			};
-		}
-	}
 
 }
